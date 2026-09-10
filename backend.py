14e386c56d43a36934abcbda4e7e99964f111676
Misbah <hassanmisbah683@gmail.com>
2026-09-10 22:48:50 +0500

    Combine backend with frontend

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 import requests
 API_KEY = "80eb2b01d6ba12a26fd2d580fa66e3f9"
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
 
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     content  = requests.get(url)
@@ -9,15 +9,10 @@ def get_data(place, forecast_days, kind):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    elif kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
     return filtered_data
 
 if __name__ == "__main__":
-    data = get_data(place="Islamabad", forecast_days=1, kind="Temperature")
+    data = get_data(place="Islamabad", forecast_days=1)
     print(data)
     print(len(data))
                     
